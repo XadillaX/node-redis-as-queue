d003dff8ca3bf953a541b6b19baea29de9b852ea
XadillaX <admin@xcoder.in>
2014-10-30 11:49:42 +0800

    unique queue finished

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * XadillaX created at 2014-10-29 17:29:56
+ *
+ * Copyright (c) 2014 Huaban.com, all rights
+ * reserved
+ */
+var BaseQueue = require("./lib/queue/base");
+var UniqueQueue = require("./lib/queue/unique");
+
+exports._BaseQueue = BaseQueue;
+exports.UniqueQueue = UniqueQueue;
+
